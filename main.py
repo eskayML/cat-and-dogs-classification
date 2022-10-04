3732207b29d178979bfb6365929ccba44eb4e10c
Samuel <kalusam619@gmail.com>
2022-10-04 12:28:42 +0100

    fix: commented my code to the fullest

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from tensorflow import keras
 from keras.models import load_model
+
+# importing the preprocessing steps for the model architecture which i used for transfer learning
 from keras.applications.xception import preprocess_input
+
 import numpy as np
 from keras.preprocessing.image import load_img, img_to_array
 import streamlit as st
 import cv2
+
+
 # import tensorflow as tf
 # print(tf.__version__)
 # print(np.__version__)
@@ -26,6 +31,7 @@ st.markdown(
 
 model = load_model('cat_and_dog_classifier.h5')
 
+
 def test_image(object_image):
     # Convert the file to an opencv image.
     file_bytes = np.asarray(bytearray(object_image.read()), dtype=np.uint8)
@@ -37,12 +43,12 @@ def test_image(object_image):
 
     if predictions[0, 0] >= 0.5:
         result = 'DOG'
-        confidence = predictions[0, 0].round(3) * 100
+        confidence = predictions[0, 0] * 100
     else:
         result = 'CAT'
-        confidence = 100 - (predictions[0, 0].round(3) * 100)
+        confidence = 100 - (predictions[0, 0] * 100)
 
-    return result, confidence
+    return result, round(confidence, 2)
     # it returns the predicted label and the precision i.e the confidence score
 
 
@@ -57,7 +63,7 @@ if submit:
         # Displaying the image
         st.image(object_image, channels="BGR")
         st.markdown(f"""## This is an image of a: {output[0]} """)
-        st.write(f'## Confidence: { output[1]}%')
+        st.write(f'# Confidence: ${ output[1]}$ %')
 
 
 # print(f'The image was predicted as a {test_image(image_path)}')
